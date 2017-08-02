e15aaf502c419b0eb5efa7961b2ea8277cbd8695
unknown <nguyenhavinh@gmail.com>
2017-08-02 18:06:48 +1000

    first commit

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>About our company</h1>
+    <p>Get more information about our services</p>
+@endsection
---
+++ resources/views/pages/careers.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Career Opportunities</h1>
+    <p>Learn more about our Driver benefits and how to become one of them</p>
+@endsection
