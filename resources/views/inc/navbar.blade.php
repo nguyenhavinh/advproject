f0bb80a37458397555b9fec0b8b23804f9f9d6e6
unknown <nguyenhavinh@gmail.com>
2017-09-08 01:47:43 +1000

    update heroku

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -47,7 +47,7 @@
                                     <li><a href="/">Driver Register</a></li>
                                 </ul>                                   
                             </li>
-                        @elseif (Auth::user())
+                        @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
